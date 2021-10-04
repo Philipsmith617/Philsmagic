bec641dca6c7242e11e3b938ec7b1e4851f98770
Philip Smith xxx <87548264+Philipsmith617@users.noreply.github.com>
2021-10-04 09:02:11 +0100

    Update Antibadword.js

--- plugins/Antibadword.js
+++ plugins/Antibadword.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2021 Queen Amdi.
+/* Copyright (C) 2021 Philsmagic✨.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Queen Amdi - Black Amda
+Philsmagic - Philip Smith
 */
 
 const Amdi = require('../events');
